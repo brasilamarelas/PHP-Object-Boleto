8d23819187253be2a00b2b7bb8b82d371e85c812
André Vitor <avandrevitor@gmail.com>
2012-07-03 10:04:05 -0300

    Correção

--- templates/html5.htm.php
+++ templates/html5.htm.php
@@ -7,7 +7,7 @@ if(!empty($OB->Banco->Css))
     $OB->Template->addStyle($OB->Banco->Css);
 
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
     <head>
         <title><?php echo $OB->Template->Title;?></title>
